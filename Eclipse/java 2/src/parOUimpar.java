398b013d43e1967e84eb77085f7dc02eeb6a82b1
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-01-27 11:07:03 -0300

    Adicionando codigos

---
+++ Eclipse/java 2/src/parOUimpar.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class parOUimpar {
+	public static void main(String[] args) {
+		int numero;
+		Scanner ler = new Scanner(System.in);
+		System.out.println("Digite um número inteiro ");
+		numero = ler.nextInt();
+		
+		
+		if( numero%2 == 0 && numero>0){
+		System.out.println("O seu número é par"); 
+		}
+		else if (numero%2 != 0 && numero>0){
+		System.out.println("Seu número é impar");
+		}
+		else if (numero < 0){
+		System.out.println("Desculpa, não consigo te ajudar");
+		}
+		else if (numero == 0){
+		System.out.println("Seu número é neutro");
+	}
+		
+	}
+
+	
+	
+}
