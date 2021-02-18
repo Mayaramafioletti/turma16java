82945dcda3380d57f4e9aabfd2c9740a62f00e3e
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-18 20:38:17 -0300

    Atualizando

--- Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ Spring/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -1,14 +1,19 @@
 package org.generation.blogPessoal.controller;
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 
@@ -37,4 +42,18 @@ public class PostagemController {
 		return ResponseEntity.ok(repositoty.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repositoty.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.OK).body(repositoty.save(postagem));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id) {
+		repositoty.deleteById(id);
+	}	
 }
